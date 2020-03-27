e1969c005ca5362ab9153b8efbe2c63d2fb405b4
Ayomide Jones <ayormeday@gmail.com>
2020-03-27 12:44:42 +0100

    initializing git

---
+++ script.js
@@ -0,0 +1,15 @@
+let name = "Nworie";
+let courses = ["html", "css", "Javascript", "ui/ux"];
+
+console.log(name);
+console.log(courses);
+
+if (courses.length % 2 === 0){
+    for (let i = 1; i <= 200; i++) {
+      if(i % 2 === 0) {
+        console.log(i);
+      }
+    }
+}
+
+
